e6963ce8d3e13062180c5fa8bc079b9f4021a182
AstroidusTv <asteroidus@protonmail.com>
2023-06-05 20:29:34 +0200

    Add first files

---
+++ target/debug/deps/tic-11331d780e240570.d
@@ -0,0 +1,5 @@
+/home/achille/Documents/Projects/Rust/tic/target/debug/deps/tic-11331d780e240570.rmeta: src/main.rs
+
+/home/achille/Documents/Projects/Rust/tic/target/debug/deps/tic-11331d780e240570.d: src/main.rs
+
+src/main.rs:
